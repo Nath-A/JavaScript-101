4cd210aefc6365aa9ae4554e9d27b06557fb3550
Nathalie Anneessens <77076065+nathalie-anneessens@users.noreply.github.com>
2022-08-15 00:39:14 +0200

    JS Fundamentals : Coding challenge #2 & #3

--- 01_JavaScript-Fundamentals/codingchallenges.js
+++ 01_JavaScript-Fundamentals/codingchallenges.js
@@ -24,5 +24,82 @@ console.log(massJohnTest2 + " / " + heightJohnTest2 + "² =" + bmiJohnTest2);
 const markHigherBMITest1 = bmiMarkTest1 > bmiJohnTest1;
 const markHigherBMITest2 = bmiMarkTest2 > bmiJohnTest2;
 
-console.log(markHigherBMITest1);
-console.log(markHigherBMITest2);
+// Coding Challenge #2
+
+//test 1 
+
+
+if (bmiMarkTest1 > bmiJohnTest1)
+{
+    console.log(`TEST 1 : Mark's BMI (${bmiMarkTest1}) is higher than John's (${bmiJohnTest1})`);
+}
+else
+{
+    console.log(`TEST 1 : John's BMI (${bmiJohnTest1}) is higher than Mark's (${bmiMarkTest1})`);
+}
+if (bmiMarkTest2 > bmiJohnTest2)
+{
+    console.log(`TEST 2 : Mark's BMI (${bmiMarkTest2}) is higher than John's (${bmiJohnTest2})`);
+}
+else
+{
+    console.log(`TEST 2 : John's BMI (${bmiJohnTest2}) is higher than Mark's (${bmiMarkTest2})`);
+}
+
+// Coding Challenge #3
+// Test 1 data
+// const DolphinsScore1 = 96;
+// const DolphinsScore2 = 108;
+// const DolphinsScore3 = 89;
+
+// const KoalasScore1 = 88;
+// const KoalasScore2 = 91;
+// const KoalasScore3 = 110;
+
+//Bonus 1 data
+// const DolphinsScore1 = 97;
+// const DolphinsScore2 = 112;
+// const DolphinsScore3 = 101;
+
+// const KoalasScore1 = 109;
+// const KoalasScore2 = 95;
+// const KoalasScore3 = 123;
+
+const DolphinsScore1 = 97;
+const DolphinsScore2 = 112;
+const DolphinsScore3 = 101;
+
+const KoalasScore1 = 109;
+const KoalasScore2 = 95;
+const KoalasScore3 = 106;
+
+const DolphinsScoreAverage = (DolphinsScore1 + DolphinsScore2 + DolphinsScore3) / 3;
+
+const KoalasScoreAverage = (KoalasScore1 + KoalasScore2 + KoalasScore3) / 3;
+
+const minScore = 100;
+
+// if (DolphinsScoreAverage >= minScore || KoalasScoreAverage >= minScore)
+if (DolphinsScoreAverage >= minScore && KoalasScoreAverage >= minScore)
+{
+    if (DolphinsScoreAverage > KoalasScoreAverage)
+    {
+        console.log(`The winner of the competition are the Dolphins ! 🏆`);
+    } else if (DolphinsScoreAverage === KoalasScoreAverage)
+    {
+        console.log(`The winner of the competition are both teams ! 🏆`);
+    }
+    else 
+    {
+        console.log(`The winner of the competition are the Koalas ! 🏆`);
+    }
+}
+else 
+{
+    console.log(`No one wins the trophy 😢`);
+}
+
+
+
+
+
